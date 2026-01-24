a0e2fe8f02dda299ee45cc048a94dd402edf1024
root <root@pale-wolf-flies-fin-03.datacrunch.io>
2026-01-24 17:47:48 +0000

    VAE 0.02

--- dataset_utils.py
+++ dataset_utils.py
@@ -24,7 +24,7 @@ def load_dataset(dataset_class, root, ds_mean, ds_std):
     train_ds = dataset_class(root=root, train=True, download=True, transform=train_transform)
     test_ds = dataset_class(root=root, train=False, transform=test_transform)
 
-    train_loader = DataLoader(train_ds, batch_size=128, shuffle=True, num_workers=24, pin_memory=True, persistent_workers=True, prefetch_factor=4)
+    train_loader = DataLoader(train_ds, batch_size=128, shuffle=True, num_workers=16, pin_memory=True, persistent_workers=True, prefetch_factor=4)
     test_loader = DataLoader(test_ds, batch_size=128, num_workers=8, pin_memory=True, persistent_workers=True, prefetch_factor=2)
 
     return train_loader, test_loader
