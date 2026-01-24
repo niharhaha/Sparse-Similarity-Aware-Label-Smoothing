15ff31ac5df2afcb3dad6be7bc7ce4fe1257de0a
root <root@pale-wolf-flies-fin-03.datacrunch.io>
2026-01-24 15:59:18 +0000

    LS 0.05 densenet

--- dataset_utils.py
+++ dataset_utils.py
@@ -24,8 +24,8 @@ def load_dataset(dataset_class, root, ds_mean, ds_std):
     train_ds = dataset_class(root=root, train=True, download=True, transform=train_transform)
     test_ds = dataset_class(root=root, train=False, transform=test_transform)
 
-    train_loader = DataLoader(train_ds, batch_size=128, shuffle=True, num_workers=32, pin_memory=True, persistent_workers=True, prefetch_factor=4)
-    test_loader = DataLoader(test_ds, batch_size=128, num_workers=16, pin_memory=True, persistent_workers=True, prefetch_factor=2)
+    train_loader = DataLoader(train_ds, batch_size=128, shuffle=True, num_workers=24, pin_memory=True, persistent_workers=True, prefetch_factor=4)
+    test_loader = DataLoader(test_ds, batch_size=128, num_workers=8, pin_memory=True, persistent_workers=True, prefetch_factor=2)
 
     return train_loader, test_loader
 
@@ -118,9 +118,9 @@ def load_tinyimagenet(root="./data/tinyimagenet"):
     )
     test_ds.class_to_idx = train_classes
 
-    train_loader = DataLoader(train_ds, batch_size=256, shuffle=True, num_workers=32, pin_memory=True, persistent_workers=True, prefetch_factor=4)
+    train_loader = DataLoader(train_ds, batch_size=256, shuffle=True, num_workers=24, pin_memory=True, persistent_workers=True, prefetch_factor=4)
 
-    test_loader = DataLoader(test_ds, batch_size=1024, shuffle=False, num_workers=16, pin_memory=True, persistent_workers=True, prefetch_factor=2)
+    test_loader = DataLoader(test_ds, batch_size=1024, shuffle=False, num_workers=8, pin_memory=True, persistent_workers=True, prefetch_factor=2)
 
     return train_loader, test_loader
 
